bf7f287259fa318efa3025a4b407c67d6c879099
MD Yasin <85479838+awesome-yasin@users.noreply.github.com>
2022-01-25 20:32:52 +0530

    Update Post.jsx

--- Instagram Downloader V2/src/components/Post.jsx
+++ Instagram Downloader V2/src/components/Post.jsx
@@ -38,7 +38,7 @@ const Post = (props) => {
         method: "GET",
         headers: {
           "x-rapidapi-key":
-            "d57e68a8eamsh96aa54d1038b6ffp17cb2djsn17a540de17a9",
+            "your-key",
           "x-rapidapi-host": "instagram-bulk-profile-scrapper.p.rapidapi.com",
         },
       }
