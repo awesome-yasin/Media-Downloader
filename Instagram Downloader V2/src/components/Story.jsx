8aafa44eca87c09f165ed1d887fc51e50d99120b
MD Yasin <85479838+awesome-yasin@users.noreply.github.com>
2022-01-25 20:34:20 +0530

    Update Story.jsx

--- Instagram Downloader V2/src/components/Story.jsx
+++ Instagram Downloader V2/src/components/Story.jsx
@@ -35,7 +35,7 @@ const Story = (props) => {
         method: "GET",
         headers: {
           "x-rapidapi-key":
-            "d57e68a8eamsh96aa54d1038b6ffp17cb2djsn17a540de17a9",
+            "your-key",
           "x-rapidapi-host": "instagram-bulk-profile-scrapper.p.rapidapi.com",
         },
       }
